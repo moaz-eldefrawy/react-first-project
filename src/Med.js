13bbe7aa2703e4a46981e0cfe251d6d82b440b47
unknown <moaz.eldefrawy@gmail.com>
2021-02-07 19:31:33 +0200

    Mod

--- src/Med.js
+++ src/Med.js
@@ -1,50 +1,82 @@
 import React from 'react'
 import Form from "react-bootstrap/Form"
+import { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Time  from "./Time";
 
 const dayName = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ,"Sunday"]
 
 function Med({medication}) {
 
+    const [name, setName] = useState("")
+    const [days, setDays] = useState([]);
+    const [time, setTime] = useState([]);
+
+    const addTime = (e) => {
+        console.log("time",e.target.value);
+        setTime( [...time, "00:00"] )
+    }
+
     return (
-        <div className ="">
+        <div className="">
             <Form>
                 <Form.Group controlId="exampleForm.ControlInput1">
-                    <Form.Label>Medication Name:</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" />
+                    <Form.Label><h4>Medication Name:</h4></Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" 
+                    value={name} onChange = { (e) => setName(e.target.value) } />
+                    
                 </Form.Group>
-                <Form.Group controlId="exampleForm.ControlSelect1">
-                    <Form.Label>Day</Form.Label>
-                    <Form.Control as="select">
-                    <option>Saturday</option>
-                    <option>Sundary</option>
-                    <option>Monday</option>
-                    <option>4</option>
-                    <option>5</option>
-                    </Form.Control>
+                
+                <Form.Group controlId="formBasicCheckbox">
+                    {
+                        dayName.map( (day) => {
+                            return <Form.Check type="checkbox" label={day} /> 
+                        } )
+                    }
                 </Form.Group>
-                <Form.Group controlId="exampleForm.ControlSelect2">
-                    <Form.Label>Example multiple select</Form.Label>
-                    <Form.Control as="select" multiple>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    </Form.Control>
+
+                <Form.Group controlId="formBasicCheckbox">
+                    <h3>Times:</h3>
+                    <Button onClick={addTime}>Add Time:</Button>
+                    {
+                        time.map( (time_,index) => {
+                            console.log(index)
+                           return <Time key={index} time={time_} 
+                           onChange = {(e) => {
+                               console.log("change")
+                               console.log(e.target)
+                               setTime( 
+                                   time.map((time_,index_) => {
+                                    console.log("target",e.target)
+                                    console.log("key",e.target.key)
+                                    if(index == e.target.key){
+                                        return e.target.value
+                                    }
+                                    else {
+                                        return time_
+                                    }
+                                    
+                                    })
+                                )
+                           }}
+                           />
+                        } )
+                    }
                 </Form.Group>
+
+                
                 <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Example textarea</Form.Label>
+                    <Form.Label><h4>Description:</h4></Form.Label>
                     <Form.Control as="textarea" rows={3} />
                 </Form.Group>
             </Form>
+
             <h1> {medication.name} </h1>
             <h3> day: {dayName[medication.day]} </h3>
             <h3> time: {medication.time} </h3>
         </div>
     )
 }
-
-
 
 
 export default Med
---
+++ src/Time.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Time(time) {
+    return (
+        <div>
+            <input type="time" value={time}/>
+        </div>
+    )
+}
+
+export default Time
