30846397df135586d4bafd1e547881e5f03c69e8
Abdallah <abdallah.yossf@gmail.com>
2021-02-07 20:28:01 +0200

    Navbar Links

--- src/reusable-components/CustomNavbar.js
+++ src/reusable-components/CustomNavbar.js
@@ -5,11 +5,20 @@ export default function CustomNavbar() {
 
     return (
         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar.Brand href="/home">Navbar</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Location</Nav.Link>
-                <Nav.Link href="#features">Summary</Nav.Link>
+                <Nav.Link href="/summary">Summary</Nav.Link>
                 <Nav.Link href="#pricing">Devices</Nav.Link>
+
+                <div style={{float:'right'}}>
+                    
+                </div>
+            </Nav>
+
+            <Nav>
+            <Nav.Link href="/login">Sign In</Nav.Link>
+            <Nav.Link href="/signup">Sign Up</Nav.Link>
             </Nav>
             
         </Navbar>
